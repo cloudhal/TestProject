751b7b5316cf70339dce431761e05538e7ef6051
cloudhal <68559542+cloudhal@users.noreply.github.com>
2026-01-20 17:52:38 +0000

    test

--- src/ConsoleApp3/Program.cs
+++ src/ConsoleApp3/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ConsoleApp1
+﻿namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World 2!");
         }
     }
 }
